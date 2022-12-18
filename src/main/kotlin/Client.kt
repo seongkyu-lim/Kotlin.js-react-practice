301255d2fc888b4618ff4a913d57d3b764491d5b
seongkyulim <96x60812@gmail.com>
2022-12-18 17:39:25 +0900

    Feature: 페이지 붉은색으로 바꿔보기

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,11 +3,5 @@ import react.create
 import react.dom.client.createRoot
 
 fun main() {
-    val container = document.createElement("div")
-    document.body!!.appendChild(container)
-
-    val welcome = Welcome.create {
-        name = "Kotlin/JS"
-    }
-    createRoot(container).render(welcome)
+    document.bgColor = "red"
 }
